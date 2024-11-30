3004c4383c67668525cc24589bdbdc08a6ee31dc
Vikalp <vickyt.1309@gmail.com>
2024-11-30 21:49:16 +0530

    Update main.py

--- multiplication-tables/main.py
+++ multiplication-tables/main.py
@@ -1,1 +1,14 @@
+print('**Welcome To Multiplication Tables**')
 
+# Main code
+while True:
+    num = float(input('\nEnter number for table: '))
+    print(f'\nMultiplication table of {num}\n' + '-'*20)
+    for multiplier in range(1, 11):
+        print(f'{num} x {multiplier} = {num * multiplier}')
+    print('-' * 20)
+    
+    if input("Continue? (Y/N): ").strip().upper() != 'Y':
+        break
+
+#FIXME: SOLVE ERRORS FOR MULTIPLICATION OF FLOATS
